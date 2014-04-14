c7299687ae55a249584ecca8f8af514a6f56b769
hwei <hwei.sy@gmail.com>
2014-04-15 03:02:18 +0800

    Draw Triangle with OpenGL 3.2 Core Profile.

--- SDL2-904/main.cpp
+++ SDL2-904/main.cpp
@@ -1,11 +1,14 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#include <OpenGL/glu.h>
+#define GL3_PROTOTYPES 1
+#include <OpenGL/gl3.h>
+//#include <SDL2/SDL_opengl.h>
+//#include <OpenGL/glu.h>
 #include <CoreFoundation/CFBundle.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <memory>
+#include <cassert>
 
 
 template <typename T, unsigned int S>
@@ -47,9 +50,9 @@ int load_resource(const char* path, void* out_buffer, size_t out_size)
 {
     auto system_encoding = CFStringGetSystemEncoding();
     auto cp_path = create_cfptr(CFStringCreateWithCString(kCFAllocatorDefault, path, system_encoding));
-    auto cp_main_bundle = create_cfptr(CFBundleGetMainBundle());
+    auto main_bundle = CFBundleGetMainBundle();
     auto cp_data_url = create_cfptr(
-        CFBundleCopyResourceURL(cp_main_bundle.get(), cp_path.get(), nullptr, nullptr));
+        CFBundleCopyResourceURL(main_bundle, cp_path.get(), nullptr, nullptr));
     auto cp_data_path = create_cfptr(CFURLCopyFileSystemPath(cp_data_url.get(), kCFURLPOSIXPathStyle));
     const char* data_path = CFStringGetCStringPtr(cp_data_path.get(), system_encoding);
     
@@ -60,7 +63,7 @@ int load_resource(const char* path, void* out_buffer, size_t out_size)
     size_t file_size = file.tellg();
     do
     {
-        if (out_size < file_size)
+        if (out_buffer == nullptr || out_size < file_size)
             break;
         file.seekg(0, file.beg);
         file.read(static_cast<char*>(out_buffer), file_size);
@@ -69,7 +72,110 @@ int load_resource(const char* path, void* out_buffer, size_t out_size)
 }
 
 
-bool loop()
+class TestShader
+{
+    bool b_valid;
+    GLuint vao;
+    GLuint vbo;
+    GLuint vertex_shader;
+    GLuint fragment_shader;
+    GLuint shader_program;
+public:
+    TestShader();
+    bool Valid() const { return this->b_valid; }
+    void Render() const;
+};
+
+TestShader::TestShader()
+: b_valid(false)
+{
+    static float vertices[] = {
+        0.0f,  0.5f, // Vertex 1 (X, Y)
+        0.5f, -0.5f, // Vertex 2 (X, Y)
+        -0.5f, -0.5f  // Vertex 3 (X, Y)
+    };
+    
+    do
+    {
+        glGenVertexArrays(1, &this->vao);
+        glBindVertexArray(this->vao);
+        
+        glGenBuffers(1, &this->vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        
+        int r;
+
+        this->vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        r = load_resource("test.vert", nullptr, 0);
+        assert(r > 0);
+        int vertex_shader_size = r;
+        std::vector<char> vertex_shader_data(512);
+        r = load_resource("test.vert", &vertex_shader_data[0], 512);
+        assert(r == vertex_shader_size);
+        const GLchar* vertex_shader_list[1];
+        vertex_shader_list[0] = &vertex_shader_data[0];
+        glShaderSource(this->vertex_shader, 1, vertex_shader_list, &vertex_shader_size);
+        glCompileShader(this->vertex_shader);
+        glGetShaderiv(this->vertex_shader, GL_COMPILE_STATUS, &r);
+        if (r != GL_TRUE)
+        {
+            char buffer[512];
+            glGetShaderInfoLog(this->vertex_shader, 512, NULL, buffer);
+            std::cerr << buffer << std::endl;
+            break;
+        }
+        
+        this->fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        r = load_resource("test.frag", nullptr, 0);
+        assert(r > 0);
+        int fragment_shader_size = r;
+        std::vector<char> fragment_shader_data(fragment_shader_size);
+        r = load_resource("test.frag", &fragment_shader_data[0], fragment_shader_size);
+        assert(r == fragment_shader_size);
+        const GLchar* fragment_shader_list[1];
+        fragment_shader_list[0] = &fragment_shader_data[0];
+        glShaderSource(this->fragment_shader, 1, fragment_shader_list, &fragment_shader_size);
+        glCompileShader(this->fragment_shader);
+        glGetShaderiv(this->fragment_shader, GL_COMPILE_STATUS, &r);
+        assert(r == GL_TRUE);
+        
+        this->shader_program = glCreateProgram();
+        glAttachShader(this->shader_program, this->vertex_shader);
+        glAttachShader(this->shader_program, this->fragment_shader);
+        
+        glLinkProgram(this->shader_program);
+        
+        GLint pos_attrib = glGetAttribLocation(this->shader_program, "position");
+        glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(pos_attrib);
+        
+        auto error = glGetError();
+        if (error != GL_NO_ERROR)
+        {
+            std::cerr << "OpenGL error: " << error << std::endl;
+        }
+        
+        this->b_valid = true;
+    } while (false);
+}
+
+void TestShader::Render() const
+{
+    GLenum error;
+    glUseProgram(this->shader_program);
+    glBindVertexArray(this->vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    error = glGetError();
+    if (error != GL_NO_ERROR)
+    {
+        std::cerr << "OpenGL error: " << error << std::endl;
+    }
+}
+
+
+
+bool loop(TestShader* p_shader)
 {
     SDL_Event e;
     while (SDL_PollEvent(&e) != 0)
@@ -85,12 +191,12 @@ bool loop()
         x = -1;
     }
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_QUADS);
-    glVertex2f( x-0.5f, -0.5f );
-    glVertex2f( x+0.5f, -0.5f );
-    glVertex2f( x+0.5f, 0.5f );
-    glVertex2f( x-0.5f, 0.5f );
-    glEnd();
+    p_shader->Render();
+    auto error = glGetError();
+    if (error != GL_NO_ERROR)
+    {
+        std::cerr << "OpenGL error: " << error << std::endl;
+    }
     return true;
 }
 
@@ -114,6 +220,10 @@ int main(int argc, char* args[])
         std::cout << std::endl;
     }
     
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    
     auto p_window = SDL_CreateWindow(
         "SDL test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         480, 640, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
@@ -130,23 +240,15 @@ int main(int argc, char* args[])
         return -3;
     }
     
-    glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-    glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-    glClearColor( 0.f, 0.f, 0.f, 1.f );
-    auto error = glGetError();
-    if (error != GL_NO_ERROR)
-    {
-        std::cerr << "OpenGL error: " << error << std::endl;
-        return -4;
-    }
     
+    TestShader test_shader;
+    assert(test_shader.Valid());
+
     typedef decltype(SDL_GetTicks()) tick_t;
     tick_t last_fps_tick = SDL_GetTicks();
     tick_t last_fps_frame = 0;
     DelayQueue<unsigned int, 6> tick_time_queue;
-    while (loop())
+    while (loop(&test_shader))
     {
         SDL_GL_SwapWindow(p_window);
         tick_t tick_5 = tick_time_queue.Head();
